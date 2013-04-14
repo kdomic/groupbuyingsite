74611b88f15381eeee125701e8591ec5db9bae00
kruno-hidden <kruno@hidden>
2013-04-15 01:49:44 +0200

    Dodane mape u ponudu

---
+++ public_html/js/main.js
@@ -0,0 +1,12 @@
+var mapOptions = {
+    center: new google.maps.LatLng(45.396838, 15.430899),
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+var markerOptions = {
+    position: new google.maps.LatLng(45.396838, 15.430899)
+};
+var marker = new google.maps.Marker(markerOptions);
+marker.setMap(map);
