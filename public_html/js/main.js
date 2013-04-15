42c59fa240fe706ff76d09a9eb72dd160a34d071
kruno-hidden <kruno@hidden>
2013-04-16 01:06:35 +0200

    Uređen slider, dodn js za izmjenu slika te detalja ponude u slideru

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,3 +1,41 @@
+/* === Slider === */
+
+function sliderImgChange(img){
+    var time = 400;
+    $('.sliderImg img').css({opacity: 1.0, visibility: "visible"}).animate({opacity: 0}, time);
+    $('.sliderImg img').delay(time+10)
+                       .queue(function(next){ 
+                            $(this).attr("src", img.src);
+                            $(this).css({opacity: 0, visibility: "visible"}).animate({opacity: 1.0}, time);
+                            next(); 
+                        });                    
+}
+
+function slideOfferChange() {
+    var time = 400;
+    $('.sliderOfferBuy h2').fadeOut(time, function() { $(this).text('1').fadeIn(time); });
+    $('.sliderOfferDiscount h2.u').fadeOut(time, function() { $(this).text('2').fadeIn(time); });
+    $('.sliderOfferDiscount h2.p').fadeOut(time, function() { $(this).text('3').fadeIn(time); });
+    $('.sliderOfferDiscount h2.v').fadeOut(time, function() { $(this).text('4').fadeIn(time); });
+    $('.sliderOfferBought h1').fadeOut(time, function() { $(this).text('5').fadeIn(time); });
+    $('.sliderOfferBought h2').fadeOut(time, function() { $(this).text('6').fadeIn(time); });
+    $('.sliderOfferBought meter').css({opacity: 1.0, visibility: "visible"}).animate({opacity: 0}, time);
+    $('.sliderOfferBought meter').delay(time+10)
+                                 .queue(function(next){ 
+                                     $('.sliderOfferBought meter').attr("value", "30");
+                                     $('.sliderOfferBought meter').attr("min", "0");
+                                     $('.sliderOfferBought meter').attr("max", "100");
+                                     $(this).css({opacity: 0, visibility: "visible"}).animate({opacity: 1.0}, time);
+                                     next(); 
+                                 });    
+    $('.sliderOfferTime h2').fadeOut(time, function() { $(this).text('7').fadeIn(time); });
+    $('.sliderOfferBought meter').attr("min", "0");
+    $('.sliderOfferBuy a').attr("href", "javascript:aletr(1)");
+    $('.sliderOfferFriend a').attr("href", "javascript:aletr(2)");    
+}
+
+/* === Google Maps === */
+
 var mapOptions = {
     center: new google.maps.LatLng(45.396838, 15.430899),
     zoom: 12,
