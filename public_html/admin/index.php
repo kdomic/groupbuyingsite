840f3ff9640216d1b66037b5b687c6c662258f77
kruno-hidden <krunoslavdomic@hotmail.com>
2013-04-22 21:52:26 +0200

    dodan jQuery tables

--- public_html/admin/index.php
+++ public_html/admin/index.php
@@ -5,10 +5,12 @@
         <title>Dobrodošli na portal za Grupnu kupovinu</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <link rel="stylesheet" type="text/css" href="../stylesheet/admin.css" />
+        <link rel="stylesheet" type="text/css" href="../stylesheet/jquery.dataTables.css" />
         <script type="text/javascript" src="../js/jquery-1.9.1.min.js"></script>
         <script type="text/javascript" src="../js/jquery.timer.js"></script>
         <script type="text/javascript" src="../js/jquery.msgBox.js"></script>
         <script type="text/javascript" src="../js/admin.js"></script> 
+        <script type="text/javascript" src="../js/jquery.dataTables.min.js"></script>
     </head>    
     
     <body>
@@ -28,7 +30,13 @@
             </div>
             <div id="content">
                 
-                
+                <div id="pocetna"></div>
+                <div id="korisnici"></div>
+                <div id="moderatori"></div>
+                <div id="kategorije"></div>
+                <div id="proizvodi"></div>
+                <div id="akcije"></div>
+                <div id="vrijeme"></div>
                                             
             </div> 
             <div class="clear"></div>        
