4a2a4af65a75841085c515a59480e2ce4cdcd3a8
kruno-hidden <krunoslavdomic@hotmail.com>
2013-04-26 00:12:45 +0200

    Dodano upravljanje akcijama

--- public_html/admin/layout_akcije.php
+++ public_html/admin/layout_akcije.php
@@ -1,1 +1,55 @@
-<div id="akcije"></div>
+<div id="akcije">
+    <div id="allActions">
+        <button id="btnNewAction">Dodaj novu akciju</button>
+        <div class="clear"></div> 
+        <table id="actionsTables">
+            <thead><tr><th>ID</th><th>Naziv</th><th>Početak</th><th>Kraj</th><th>Istaknut</th><th>Aktivan</th></tr></thead>
+            <tbody></tbody>
+        </table>
+    </div>
+    <div id="singleAction">
+        <table>
+            <tr>
+                <td><label for="actionID">ID</label></td>
+                <td><input type="text"      name="id"      id="actionID"    readonly /></td>
+            </tr>
+            <tr>
+                <td><label for="actionPONUDA">Ponuda</label></td>
+                <td><select name="ponuda"      id="actionPONUDA"></select></td>
+            </tr>                                        
+            <tr>
+                <td><label for="actionPOPUST">Popust</label></td>
+                <td><input type="text"      name="popust"      id="actionPOPUST"      placeholder=""/></td>
+            </tr>
+            <tr>
+                <td><label for="actionPOCETAK">Početak</label></td>
+                <td><input type="datetime"      name="pocetak"      id="actionPOCETAK"      placeholder=""/></td>
+            </tr>
+            <tr>
+                <td><label for="actionZAVRSETAK">Završetak</label></td>
+                <td><input type="datetime"      name="zavrsetak"      id="actionZAVRSETAK"      placeholder=""/></td>
+            </tr>
+            <tr>
+                <td><label for="actionGRANICA">Granica</label></td>
+                <td><input type="text"      name="limit"      id="actionGRANICA"      placeholder=""/></td>
+            </tr>
+            <tr>
+                <td><label for="actionISTAKNUTO">Istaknuto</label></td>
+                <td>
+                    <input type="radio" name="istaknuto" value="1" id="actionISTAKNUTO">Da
+                    <input type="radio" name="istaknuto" value="0" >Ne
+                </td>
+            </tr>
+            <tr>
+                <td><label for="actionAKTIVNA">Vidljivost</label></td>
+                <td>
+                    <input type="radio" name="vidljivost" value="1" id="actionAKTIVNA">Da
+                    <input type="radio" name="vidljivost" value="0" >Ne
+                </td>
+            </tr>                                                        
+            <tr>
+            <td colspan="2"><button id="btnSaveAction">Pospremi</button></td>
+            </tr>                            
+        </table>
+    </div>
+</div>
---
+++ public_html/includes/akcije.php
@@ -0,0 +1,72 @@
+<?php
+    require_once('initialize.php');
+    
+    class Akcije extends DatabaseObject {
+        protected static $table_name = "akcije";
+        protected static $db_fields = array('id',
+                                            'id_ponude',
+                                            'popust',
+                                            'datum_pocetka',
+                                            'datum_zavrsetka',
+                                            'granica',
+                                            'istaknuto',
+                                            'aktivan');
+            public $id;                 //[0]
+            public $id_ponude;          //[1]
+            public $popust;             //[2]
+            public $datum_pocetka;      //[3]
+            public $datum_zavrsetka;    //[4]
+            public $granica;            //[5]
+            public $istaknuto;          //[6]
+            public $aktivan;            //[7]
+            
+            public static function getAll() //1
+            {
+                $_d = self::find_all();
+                $xmlDoc = new DOMDocument();
+                $root = $xmlDoc->appendChild($xmlDoc->createElement("akcije"));
+                foreach ($_d as $d){
+                    $data = $root->appendChild($xmlDoc->createElement("akcija"));
+                    $ponuda = Ponude::find_by_id($d->id_ponude);
+                    $d->id_ponude = $ponuda->naslov;
+                    $d->istaknuto = $d->istaknuto==1 ? "Da" : "Ne";
+                    $d->aktivan = $d->aktivan==1 ? "Da" : "Ne";
+                    foreach ($d as $key => $value) {
+                        $data->appendChild($xmlDoc->createElement($key, htmlentities($value)));
+                    }
+                }
+                header("Content-Type: text/xml");
+                $xmlDoc->formatOutput = true;
+                echo $xmlDoc->saveXML();
+            }
+
+            public static function get($id) //2
+            {
+                $d = self::find_by_id($id);
+                $xmlDoc = new DOMDocument();
+                $root = $xmlDoc->appendChild($xmlDoc->createElement("akcije"));
+                $data = $root->appendChild($xmlDoc->createElement("akcija"));
+                foreach ($d as $key => $value) {
+                    $data->appendChild($xmlDoc->createElement($key, htmlentities($value)));
+                }
+                header("Content-Type: text/xml");
+                $xmlDoc->formatOutput = true;
+                echo $xmlDoc->saveXML();
+            }
+
+            public static function set($data) //3,4
+            {                
+                $d = new Akcije();
+                if((int)$data[0]===4) $d = self::find_by_id($data[1]);            
+                $d->aktivan = array_pop($data);
+                $d->istaknuto = array_pop($data);
+                $d->granica = array_pop($data);
+                $d->datum_zavrsetka = timeForMysql(array_pop($data));
+                $d->datum_pocetka = timeForMysql(array_pop($data));
+                $d->popust = array_pop($data);
+                $d->id_ponude = array_pop($data);
+                xmlStatusSend($d->save());
+            }
+
+    }
+?>
