ef818edaa30e44115597adc463e65bf95ee2870f
kruno-hidden <krunoslavdomic@hotmail.com>
2013-04-26 00:12:14 +0200

    razno

--- public_html/test.php
+++ public_html/test.php
@@ -1,17 +1,35 @@
 <?php require_once('includes/initialize.php'); ?>
 <pre>
 	<?php
-		$ponude = Ponude::find_all();
-		foreach ($ponude as $p) {
-			$nova = Ponude::find_by_id($p->id);
-			foreach ($p as $key => $value) {
-				$nova->$key = strip_tags($value);
-				//$value = strip_tags($value);
-				//print_r($key);
-			}
-			$nova->save();
-			//print_r($p);
-		}
-		echo "kraj";;
+		$ponude = Gradovi_akcije::find_all();
+		print_r($ponude);		
 	?>
 </pre>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php /*
+	$ponude = Ponude::find_all();
+	foreach ($ponude as $p) {
+		$nova = Ponude::find_by_id($p->id);
+		foreach ($p as $key => $value) {
+			$nova->$key = strip_tags($value);
+			//$value = strip_tags($value);
+			//print_r($key);
+		}
+		$nova->save();
+		//print_r($p);
+	}
+	echo "kraj";;*/
+?>
