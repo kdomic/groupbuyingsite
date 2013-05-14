09312ab36c06aa32f3ab5aec94b02587ef0b2ec5
kruno-hidden <krunoslavdomic@hotmail.com>
2013-05-14 21:44:52 +0200

    Tipfeler izmjena

--- public_html/admin/layout_vrijeme.php
+++ public_html/admin/layout_vrijeme.php
@@ -2,7 +2,7 @@
 	<h1>Postavke sustava</h1>
 	<p>Trenutni pomak: <b><span id="currentTimeOffset">4</span></b></p>
 	<p>Pomak vremena moguće je upisati: <a href="http://arka.foi.hr/PzaWeb/PzaWeb2004/config/vrijeme.html" target="_blank">ovdje</a></p>
-	<p>Nakon promjene vremena pritisnite gum za preuzimanje</p>
+	<p>Nakon promjene vremena pritisnite gumb za preuzimanje</p>
 	<button id="btnSaveTime">Preuzmi vrijeme</button>
 	<br><hr><br>
 	<div class="clear"></div> 
