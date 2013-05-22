3a68394e3b11bb07580cf397cab2f2dc5e22a3c9
kruno-hidden <krunoslavdomic@hotmail.com>
2013-05-22 17:32:30 +0200

    Dodavanje dokumentacije

--- public_html/layout_footer.php
+++ public_html/layout_footer.php
@@ -1,3 +1,18 @@
 <footer>	
-    Footer
+    <div class="footerContent">
+    	<div class="divFooterLeft">
+    		Stranica u potpunosti izrađena od strane: <br><br>
+    		<b>Krunoslav Domić</b><br>
+    		kdomic@foi.hr<br><br>
+    		Stranica izrađena u sklupu projekta za kolegij:<br>
+    		Web dizajn i programiranje  		
+    	</div>
+    	<div class="divFooterLeft">
+    		Detaljnu dokumentaciju portala, korištenih dodataka, te
+    		<br> vanjske reference možete pronaći u dokumentaciji portala. <br><br>
+    		<a href="dokumentacija.html">Dokumentacija portala</a>
+    	</div>
+    	<div class="clear"></div>
+    	<br><br><br>Copyright © 2013 Krunoslav Domić - kdomic@foi.hr Sva prava pridržana.	
+    </div>
 </footer>
