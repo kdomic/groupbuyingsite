2a9e2c8f02c01d8c8c5c6341c89ff4bb9e5e14bd
kruno-hidden <krunoslavdomic@hotmail.com>
2013-05-25 12:55:22 +0200

    baza final

--- WebDiP2012_013_kostur.sql
+++ WebDiP2012_013_kostur.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
+-- version 3.3.7deb7
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 03, 2013 at 09:15 PM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
+-- Generation Time: May 25, 2013 at 12:54 PM
+-- Server version: 5.0.51
+-- PHP Version: 5.3.3-7+squeeze15
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -27,17 +26,17 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `akcije` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `id_ponude` int(11) NOT NULL,
   `popust` int(11) NOT NULL,
   `datum_pocetka` datetime NOT NULL,
   `datum_zavrsetka` datetime NOT NULL,
   `granica` int(11) NOT NULL,
-  `istaknuto` tinyint(4) DEFAULT '0' COMMENT '0 ne\n1 da',
-  `aktivan` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
+  `istaknuto` tinyint(4) default '0' COMMENT '0 ne\n1 da',
+  `aktivan` tinyint(1) NOT NULL default '1',
+  PRIMARY KEY  (`id`),
   KEY `fk_akcije_1` (`id_ponude`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=53 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -46,11 +45,11 @@ CREATE TABLE IF NOT EXISTS `akcije` (
 --
 
 CREATE TABLE IF NOT EXISTS `gradovi` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `ime` varchar(45) NOT NULL,
-  `aktivan` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+  `aktivan` tinyint(1) NOT NULL default '1',
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -59,13 +58,13 @@ CREATE TABLE IF NOT EXISTS `gradovi` (
 --
 
 CREATE TABLE IF NOT EXISTS `gradovi_akcije` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `id_grada` int(11) NOT NULL,
   `id_akcije` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY  (`id`),
   KEY `fk_gradovi_akcije_1` (`id_grada`),
   KEY `fk_gradovi_akcije_2` (`id_akcije`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=104 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -74,11 +73,11 @@ CREATE TABLE IF NOT EXISTS `gradovi_akcije` (
 --
 
 CREATE TABLE IF NOT EXISTS `kategorije` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `naziv` varchar(45) NOT NULL,
-  `aktivan` tinyint(4) NOT NULL DEFAULT '1' COMMENT '0 neaktivna1 aktivna',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=25 ;
+  `aktivan` tinyint(4) NOT NULL default '1' COMMENT '0 neaktivna1 aktivna',
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -87,17 +86,17 @@ CREATE TABLE IF NOT EXISTS `kategorije` (
 --
 
 CREATE TABLE IF NOT EXISTS `komentari` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `id_korisnika` int(11) NOT NULL,
   `id_ponude` int(11) NOT NULL,
   `komentar` text NOT NULL,
   `ocjena` int(11) NOT NULL,
   `datum` datetime NOT NULL,
-  `aktivan` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
+  `aktivan` tinyint(1) NOT NULL default '1',
+  PRIMARY KEY  (`id`),
   KEY `fk_komentari_1` (`id_korisnika`),
   KEY `fk_komentari_2` (`id_ponude`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -106,26 +105,26 @@ CREATE TABLE IF NOT EXISTS `komentari` (
 --
 
 CREATE TABLE IF NOT EXISTS `korisnici` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `ime` varchar(45) NOT NULL,
   `prezime` varchar(45) NOT NULL,
   `adresa` varchar(45) NOT NULL,
   `pbr` varchar(5) NOT NULL,
   `mjesto` varchar(45) NOT NULL,
-  `telefon` varchar(20) DEFAULT NULL,
+  `telefon` varchar(20) default NULL,
   `email` varchar(45) NOT NULL,
   `oib` varchar(11) NOT NULL,
-  `open_id` mediumint(9) DEFAULT '0' COMMENT '0 - normalno\n1- facebook\n2 - google',
-  `deaktiviran` tinyint(4) DEFAULT '0' COMMENT '0 aktiviran\n1 deaktiviran',
-  `zamrznut` datetime DEFAULT NULL COMMENT 'Datum do kojeg je aktiviran',
-  `datum_registracije` datetime DEFAULT NULL,
-  `email_potvrda` varchar(45) DEFAULT NULL,
+  `open_id` mediumint(9) default '0' COMMENT '0 - normalno\n1- facebook\n2 - google',
+  `deaktiviran` tinyint(4) default '0' COMMENT '0 aktiviran\n1 deaktiviran',
+  `zamrznut` datetime default NULL COMMENT 'Datum do kojeg je aktiviran',
+  `datum_registracije` datetime default NULL,
+  `email_potvrda` varchar(45) default NULL,
   `password` varchar(45) NOT NULL,
   `ovlasti` mediumint(9) NOT NULL COMMENT '1 reg_user\n2 mod\n3 admin',
-  `aktivan` tinyint(4) NOT NULL DEFAULT '1' COMMENT '0-ne; 1-da',
-  PRIMARY KEY (`id`),
+  `aktivan` tinyint(4) NOT NULL default '1' COMMENT '0-ne; 1-da',
+  PRIMARY KEY  (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=41 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -134,15 +133,15 @@ CREATE TABLE IF NOT EXISTS `korisnici` (
 --
 
 CREATE TABLE IF NOT EXISTS `kosarica` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `id_korisnika` int(11) NOT NULL,
   `id_akcije` int(11) NOT NULL,
-  `operacija` mediumint(9) NOT NULL DEFAULT '0' COMMENT '0 dodano\n1 izbaceno\n2 kupljeno',
+  `operacija` mediumint(9) NOT NULL default '1' COMMENT '1dodano; 2 kupljeno',
   `datum` datetime NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY  (`id`),
   KEY `fk_kosarica_1` (`id_korisnika`),
   KEY `fk_kosarica_2` (`id_akcije`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -151,15 +150,15 @@ CREATE TABLE IF NOT EXISTS `kosarica` (
 --
 
 CREATE TABLE IF NOT EXISTS `logovi` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `id_korisnika` int(11) NOT NULL,
   `id_tip` int(11) NOT NULL COMMENT 'tip zapisa',
-  `kljuc` int(11) NOT NULL,
+  `kljuc` varchar(10) NOT NULL,
   `datum` datetime NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY  (`id`),
   KEY `fk_logovi_1` (`id_korisnika`),
   KEY `fk_logovi_2` (`id_tip`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=68 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -168,10 +167,10 @@ CREATE TABLE IF NOT EXISTS `logovi` (
 --
 
 CREATE TABLE IF NOT EXISTS `log_tip` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `opis` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=31 ;
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -180,13 +179,13 @@ CREATE TABLE IF NOT EXISTS `log_tip` (
 --
 
 CREATE TABLE IF NOT EXISTS `moderatori` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `id_korisnika` int(11) NOT NULL,
   `id_kategorije` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY  (`id`),
   KEY `fk_moderatori_1` (`id_korisnika`),
   KEY `fk_moderatori_2` (`id_kategorije`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -195,14 +194,14 @@ CREATE TABLE IF NOT EXISTS `moderatori` (
 --
 
 CREATE TABLE IF NOT EXISTS `newsletter` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `id_korisnika` int(11) NOT NULL,
   `email` varchar(100) NOT NULL,
   `kategorija` int(11) NOT NULL,
   `aktivan` tinyint(4) NOT NULL COMMENT '0 neaktivan\n1 aktivan',
-  PRIMARY KEY (`id`),
+  PRIMARY KEY  (`id`),
   KEY `fk_newsletter_1` (`id_korisnika`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -211,15 +210,15 @@ CREATE TABLE IF NOT EXISTS `newsletter` (
 --
 
 CREATE TABLE IF NOT EXISTS `opomene` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `id_korisnika` int(11) NOT NULL,
   `id_moderatora` int(11) NOT NULL,
   `datum` datetime NOT NULL,
   `opis` text NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY  (`id`),
   KEY `fk_opomene_1` (`id_korisnika`),
   KEY `fk_opomene_2` (`id_moderatora`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -228,7 +227,7 @@ CREATE TABLE IF NOT EXISTS `opomene` (
 --
 
 CREATE TABLE IF NOT EXISTS `ponude` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `id_prodavatelja` int(11) NOT NULL,
   `id_kategorije` int(11) NOT NULL,
   `naslov` text NOT NULL,
@@ -238,13 +237,13 @@ CREATE TABLE IF NOT EXISTS `ponude` (
   `opis_kratki` text NOT NULL,
   `opis` text NOT NULL,
   `napomena` text NOT NULL,
-  `karta_x` varchar(45) DEFAULT NULL,
-  `karta_y` varchar(45) DEFAULT NULL,
-  `aktivan` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
+  `karta_x` varchar(45) default NULL,
+  `karta_y` varchar(45) default NULL,
+  `aktivan` tinyint(1) NOT NULL default '1',
+  PRIMARY KEY  (`id`),
   KEY `fk_ponude_1` (`id_prodavatelja`),
   KEY `fk_ponude_2` (`id_kategorije`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=48 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -253,7 +252,7 @@ CREATE TABLE IF NOT EXISTS `ponude` (
 --
 
 CREATE TABLE IF NOT EXISTS `prodavatelji` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `id_korisnika` int(11) NOT NULL COMMENT 'korisnik koji je zaduzen za to poduc+zece',
   `naziv` varchar(45) NOT NULL,
   `adresa` varchar(45) NOT NULL,
@@ -261,9 +260,9 @@ CREATE TABLE IF NOT EXISTS `prodavatelji` (
   `info` varchar(45) NOT NULL,
   `oib` varchar(45) NOT NULL,
   `aktivan` tinyint(4) NOT NULL COMMENT '0 neaktivan\n1 aktivan',
-  PRIMARY KEY (`id`),
+  PRIMARY KEY  (`id`),
   KEY `fk_prodavatelji_1` (`id_korisnika`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=46 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -272,13 +271,13 @@ CREATE TABLE IF NOT EXISTS `prodavatelji` (
 --
 
 CREATE TABLE IF NOT EXISTS `racuni` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `id_korisnika` int(11) NOT NULL,
   `datum` datetime NOT NULL,
   `placeno` tinyint(4) NOT NULL COMMENT '0 ne\n1 da',
-  PRIMARY KEY (`id`),
+  PRIMARY KEY  (`id`),
   KEY `fk_racuni_1` (`id_korisnika`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=27 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -287,33 +286,14 @@ CREATE TABLE IF NOT EXISTS `racuni` (
 --
 
 CREATE TABLE IF NOT EXISTS `racuni_akcije` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `id_racuna` int(11) NOT NULL,
   `id_akcije` int(11) NOT NULL,
   `kolicina` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY  (`id`),
   KEY `fk_racuni_akcije_1` (`id_racuna`),
   KEY `fk_racuni_akcije_2` (`id_akcije`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=22 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `vaucheri`
---
-
-CREATE TABLE IF NOT EXISTS `vaucheri` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_korisnika` int(11) NOT NULL,
-  `ime` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `poruka` text NOT NULL,
-  `vrijednost` int(11) NOT NULL,
-  `id_racuna` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_vaucheri_1` (`id_korisnika`),
-  KEY `fk_vaucheri_2` (`id_racuna`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -322,10 +302,10 @@ CREATE TABLE IF NOT EXISTS `vaucheri` (
 --
 
 CREATE TABLE IF NOT EXISTS `vrijeme` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `pomak` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 --
 -- Constraints for dumped tables
@@ -410,14 +390,3 @@ ALTER TABLE `racuni`
 ALTER TABLE `racuni_akcije`
   ADD CONSTRAINT `fk_racuni_akcije_1` FOREIGN KEY (`id_racuna`) REFERENCES `racuni` (`id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_racuni_akcije_2` FOREIGN KEY (`id_akcije`) REFERENCES `akcije` (`id`) ON UPDATE CASCADE;
-
---
--- Constraints for table `vaucheri`
---
-ALTER TABLE `vaucheri`
-  ADD CONSTRAINT `fk_vaucheri_1` FOREIGN KEY (`id_korisnika`) REFERENCES `korisnici` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_vaucheri_2` FOREIGN KEY (`id_racuna`) REFERENCES `racuni` (`id`) ON UPDATE CASCADE;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
